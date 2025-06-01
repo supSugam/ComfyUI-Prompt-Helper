36016480c4ae2d03159a7e1d34d2b1649fa680b9
supSugam <sugamsubedi234@gmail.com>
2025-06-01 18:55:53 +0545

    feat: add metadata for comfyui node (scaffold)

--- __init__.py
+++ __init__.py
@@ -1,4 +1,15 @@
+"""Top-level package for prompt_helper."""
+
 from .nodes.image_to_prompt import ImageToPrompt
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+]
+
+__author__ = """Sugam"""
+__email__ = "sugamsubedi234@gmail.com"
+__version__ = "0.0.1"
 
 NODE_CLASS_MAPPINGS = {
     "ImageToPrompt": ImageToPrompt,
